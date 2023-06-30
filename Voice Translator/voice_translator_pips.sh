e2177c5e0d0a7c269f18eed6143043743a724529
André Coutinho de Macedo Silva <91290799+AndreCoutinhom@users.noreply.github.com>
2023-06-29 23:24:46 -0300

    Update voice_translator_pips.sh

--- Voice Translator/voice_translator_pips.sh
+++ Voice Translator/voice_translator_pips.sh
@@ -1,4 +1,13 @@
+#!/bin/bash
+
+# Instalação da biblioteca SpeechRecognition
 pip install SpeechRecognition
+
+# Instalação da biblioteca pyttsx3
 pip install pyttsx3
+
+# Instalação da biblioteca langdetect
 pip install langdetect
+
+# Instalação da biblioteca googletrans (versão específica)
 pip install --upgrade googletrans==4.0.0-rc1
