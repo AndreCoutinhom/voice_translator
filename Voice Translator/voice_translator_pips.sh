d37b80c3a7457f41c80dae891cf6e3c3bc351c33
André Coutinho de Macedo Silva <91290799+AndreCoutinhom@users.noreply.github.com>
2023-08-07 14:29:50 -0300

    Atualizar o voice_translator_pips.sh

--- Voice Translator/voice_translator_pips.sh
+++ Voice Translator/voice_translator_pips.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# SpeechRecognition
+# SpeechRecognition - Reconhecimento de fala
 pip install SpeechRecognition
 
-# pyttsx3
+# pyttsx3 - versão python mais recente no desenvolvimento do código 
 pip install pyttsx3
 
-# langdetect
+# langdetect - detecção de idioma
 pip install langdetect
 
-# googletrans (version 4.0.0)
+# googletrans (version 4.0.0) - versão mais recente no desenvolvimento do código 
 pip install googletrans-py
 pip install --upgrade googletrans==4.0.0-rc1
